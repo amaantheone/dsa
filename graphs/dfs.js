e1caf98841421552325b134c9869fa832d5f6b79
calmsekaam <amaantheone@gmail.com>
2026-03-14 22:57:01 +0530

    implemented dfs traversal

---
+++ graphs/dfs.js
@@ -0,0 +1,32 @@
+const graph = {
+    a: [ 'b', 'c' ],
+    b: [],
+    c: [ 'e', 'd' ],
+    d: [ 'f' ],
+    e: [],
+    f: []
+};
+
+// const depthFirstSearch = (graph, source) => {
+//     stack = [ source ];
+    
+//     while ( stack.length > 0 ) {
+//         const current = stack.pop();
+//         console.log(current);
+
+//         for ( let neighbour of graph[current] ) {
+//             stack.push(neighbour);
+//         }
+//     }
+// };
+
+// recursive
+const depthFirstSearch = (graph, source) => {
+    console.log(source);
+    for (let neighbour of graph[source]) {
+        depthFirstSearch(graph, neighbour);
+    }
+}
+
+
+depthFirstSearch(graph, 'a');
