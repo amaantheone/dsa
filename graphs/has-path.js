a28529b2b272fe951c75aa944a5648771325545f
calmsekaam <amaantheone@gmail.com>
2026-03-18 17:38:42 +0530

    add: undirected-path.js

---
+++ graphs/has-path.js
@@ -0,0 +1,40 @@
+const graph = {
+  f: ['g', 'i'],
+  g: ['h'],
+  h: [],
+  i: ['g', 'k'],
+  j: ['i'],
+  k: []
+};
+
+// bfs
+const hasPath = (graph, src, dst) => {
+  const queue = [src];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current === dst) return true;
+
+    for ( let neighbour of graph[current]) {
+      queue.push(neighbour);
+    }
+  }
+  return false;
+}
+
+// dfs
+// const hasPath = (graph, src, dst) => {
+//   if (src === dst) return true;
+
+//   for ( let neighbour of graph[src] ) {
+//     if ( hasPath(graph, neighbour, dst) === true ) {
+//       return true;
+//     }
+//   }
+  
+//   return false;
+// }
+
+const res = hasPath(graph, 'f', 'k'); // true
+const res1 = hasPath(graph, 'k', 'h'); // false
+console.log(`${res}\n${res1}`);
